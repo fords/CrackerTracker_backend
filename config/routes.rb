4aa5befcc8b67160179c2e7e025bfa733277fdae
fords <zxw6805@rit.edu>
2018-12-10 10:39:48 -0500

    default page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-root "application#index"
+
 
 Rails.application.routes.draw do
   resources :foods, except: %i[new edit]
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   post '/foods' => 'foods#create'
   delete '/foods/' => 'foods#destroy'
   patch '/foods' => 'foods#update'
-
+  root "application#index"
   # RESTful routes
   resources :examples, except: %i[new edit]
   resources :users, only: %i[index show update create]
